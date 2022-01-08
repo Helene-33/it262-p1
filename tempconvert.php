cd90bee526ed6ae6f8faf8cfb0199e52fc7ba6ce
Helene Liobard <77423175+Helene-33@users.noreply.github.com>
2022-01-08 15:26:12 -0800

    review code + add some style

--- tempconvert.php
+++ tempconvert.php
@@ -1,44 +1,29 @@
 <?php
-
 /*
-
 Celsius to Fahrenheit	° F = 9/5 ( ° C) + 32
-
         example: 20 degrees Cel = 68 degrees Far
             20 * 1.8 (or 9/5) = 36 + 32 = 68
 
-
 Fahrenheit to Celsius	° C = 5/9 (° F - 32)
-
         example: 90 degrees Far = 32.2 degrees Cel
             (90 - 32 = 58) .5556 * 58 = 32.2
 
-
 Kelvin to Fahrenheit	° F = 9/5 (K - 273) + 32
-
         example: 60 degrees Kel = -351.4 degrees Far
             (60 - 273 = -213) * 1.8 = -384.4 + 32 = -351.4
 
-
 Fahrenheit to Kelvin	K = 5/9 (° F - 32) + 273
-
         example: 40 degrees Far = 277.448 degrees Kel
             .5556 (40 - 32 = 8) 4.4448 + 273 = 277.448
 
 Celsius to Kelvin	    K = ° C + 273
-
         example: 32 degrees Cel = 305 degrees Kel
             32 + 273 = 305
 
-
 Kelvin to Celsius	    ° C = K - 273
-
         example: 500 degrees Kel = 227 degrees Cel
             500 - 273 = 227
-
-
 */
-
 ?>
 
 <!doctype html>
@@ -48,27 +33,27 @@ Kelvin to Celsius	    ° C = K - 273
 <meta name="viewport" content="width=device-width, initial-scale=1.0" >
 <link href="css/styles.css" type="text/css" rel= "stylesheet">
 
-<title>Temp Conversion Calculator</title>
+<title>Temperature Conversion Calculator</title>
 </head>
 
 <body>
 <div class="wrapper">
 
-<h1>Temp Converter yay!</h1>
+<h1>Temperature Converter yay!</h1>
 <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
-<fieldset>
+<fieldset class="fieldset">
 
-<label for="temp">Enter The Temperature You Wish to Convert</label>
+<label for="temp">Enter The Temperature You Wish to Convert:</label>
 <input for="temp" name="temp" id="temp" type="number" value="<?php if(isset($_POST['temp'])) echo htmlspecialchars($_POST['temp']) ;?>">
 
-<label for="unit_1">Converting From</label>
+<label for="unit_1">Converting From:</label>
 <ul>
 <li><input type="radio" name="unit_1" value="Fahrenheit">Fahrenheit</li>
 <li><input type="radio" name="unit_1" value="Celsius">Celsius</li>
 <li><input type="radio" name="unit_1" value="Kelvin">Kelvin</li>
 </ul>
 
-<label for="unit_2">Converting To</label>
+<label for="unit_2">Converting To:</label>
 <ul>
 <li><input type="radio" name="unit_2" value="Fahrenheit">Fahrenheit</li>
 <li><input type="radio" name="unit_2" value="Celsius">Celsius</li>
@@ -83,104 +68,93 @@ Kelvin to Celsius	    ° C = K - 273
 
 <?php
 // if($_SERVER['REQUEST_METHOD'] == 'POST' && isset(
-
 //     $_POST['unit_1'],
 //     $_POST['unit_2'],
 //     $_POST['temp']
 //  )) {
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
-
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
     // $unit_1 = $_POST['unit_1'];
     // $unit_2 = $_POST['unit_2'];
     // $temp = intval($_POST['temp']);
     // $converted_temp = 0; 
     $errorMsg = "";
 
-    if(empty($_POST['unit_1']) && empty($_POST['unit_2']) ) { // if both are empty
-        $errorMsg .= '<span class="error">Please select your both your units of measurement!</span>';
-    }
+if(empty($_POST['unit_1']) && empty($_POST['unit_2']) ) { // if both are empty
+    $errorMsg .= '<span class="error">Please select both your units of measurement! <br></span>';
+}
+
+if(empty($_POST['unit_1']) && isset($_POST['unit_2'])) { // if unit 1 is null but unit 2 is good
+    $errorMsg .= '<span class="error">Please select your unit of measurement! <br></span>';
+}
+    
+if(empty($_POST['unit_2']) && isset($_POST['unit_1'])) { // if unit 2 is null but unit 1 is good
+    $errorMsg .= '<span class="error">What are we converting to? <br></span>';
+}
+
+if($_POST['temp'] === "") { // if temp is empty
+    $errorMsg .= '<span class="error">Please fill out a temperature! <br></span>';
+}
 
     
+if(isset(
+    $_POST['unit_1'],
+    $_POST['unit_2'],
+    $_POST['temp']
+)) {
+    $unit_1 = $_POST['unit_1'];
+    $unit_2 = $_POST['unit_2'];
+    $converted_temp = 0; 
+    $temp = intval($_POST['temp']); 
 
-    if(empty($_POST['unit_1']) && isset($_POST['unit_2'])) { // if unit 1 is null but unit 2 is good
-        $errorMsg .= '<p>Please select your unit of measurement!<p>';
-    }
+if($unit_1 === $unit_2) { // if both are the same conversion
+    $errorMsg .= '<p>Please Select Two Different Units!</p>';
+}
     
+if($unit_1 == 'Fahrenheit' && $unit_2 == 'Celsius') { // Fahrenheit to Celsius	° C = 5/9 (° F - 32)
+    $converted_temp = 5/9 * ($temp - 32);
+}
 
-    if(empty($_POST['unit_2']) && isset($_POST['unit_1'])) { // if unit 2 is null but unit 1 is good
-        $errorMsg .= '<span class="error">What are we converting to?</span>';
-    }
+if($unit_1 == 'Celsius' && $unit_2 == 'Fahrenheit') { // Celsius to Fahrenheit	° F = 9/5 ( ° C) + 32
+    $converted_temp = 9/5 * $temp + 32;
+}
 
+if($unit_1 == 'Kelvin' && $unit_2 == 'Fahrenheit') { // Kelvin to Fahrenheit	° F = 9/5 (K - 273) + 32
+    $converted_temp = 9/5 * ($temp - 273) + 32;
+}
 
-    if($_POST['temp'] === "") { // if temp is empty
-        $errorMsg .= '<span class="error">Please fill out your temp!</span>';
-    }
+if($unit_1 == 'Fahrenheit' && $unit_2 == 'Kelvin') { // Fahrenheit to Kelvin	K = 5/9 (° F - 32) + 273
+    $converted_temp = 5/9 * ($temp - 32) + 273;
+}
 
-    
+if($unit_1 == 'Kelvin' && $unit_2 == 'Celsius') { // Kelvin to Celsius	 ° C = K - 273
+    $converted_temp = $temp - 273;
+}
 
+if($unit_1 == 'Celsius' && $unit_2 == 'Kelvin') { // Celsius to Kelvin	 K = ° C + 273
+    $converted_temp = $temp + 273;
+} 
+    // echo '
+    // <div class="result">
+    // <h2>Converted Temp</h2>
+    // <p>'.$temp.' degrees '.$unit_1.' is equal to '.number_format($converted_temp, 2).' degrees '.$unit_2.'. Good Day!</p>
+    // </div>';
 
-   if(isset(
-      $_POST['unit_1'],
-      $_POST['unit_2'],
-      $_POST['temp']
-   )) {
-     $unit_1 = $_POST['unit_1'];
-     $unit_2 = $_POST['unit_2'];
-     $converted_temp = 0; 
-     $temp = intval($_POST['temp']); 
+// if two of the same units are selected
+// if(isset($_POST['unit_1']) === $_POST['unit_2']) {
+//     echo '<p>Please Select Two Different Units!</p>';
+// }
 
-        if($unit_1 === $unit_2) { // if both are the same conversion
-        $errorMsg .= '<p>Please Select Two Different Units!</p>';
-        }
-    
-        if($unit_1 == 'Fahrenheit' && $unit_2 == 'Celsius') { // Fahrenheit to Celsius	° C = 5/9 (° F - 32)
-            $converted_temp = 5/9 * ($temp - 32);
-        }
+} // end of isset if statment
 
-        if($unit_1 == 'Celsius' && $unit_2 == 'Fahrenheit') { // Celsius to Fahrenheit	° F = 9/5 ( ° C) + 32
-            $converted_temp = 9/5 * $temp + 32;
-        }
-
-        if($unit_1 == 'Kelvin' && $unit_2 == 'Fahrenheit') { // Kelvin to Fahrenheit	° F = 9/5 (K - 273) + 32
-            $converted_temp = 9/5 * ($temp - 273) + 32;
-        }
-
-        if($unit_1 == 'Fahrenheit' && $unit_2 == 'Kelvin') { // Fahrenheit to Kelvin	K = 5/9 (° F - 32) + 273
-            $converted_temp = 5/9 * ($temp - 32) + 273;
-        }
-
-        if($unit_1 == 'Kelvin' && $unit_2 == 'Celsius') { // Kelvin to Celsius	 ° C = K - 273
-            $converted_temp = $temp - 273;
-        }
-
-        if($unit_1 == 'Celsius' && $unit_2 == 'Kelvin') { // Celsius to Kelvin	 K = ° C + 273
-            $converted_temp = $temp + 273;
-        } 
-            // echo '
-            // <div class="result">
-            // <h2>Converted Temp</h2>
-            // <p>'.$temp.' degrees '.$unit_1.' is equal to '.number_format($converted_temp, 2).' degrees '.$unit_2.'. Good Day!</p>
-            // </div>';
-
-        
-            // if two of the same units are selected
-            // if(isset($_POST['unit_1']) === $_POST['unit_2']) {
-            //     echo '<p>Please Select Two Different Units!</p>';
-            // }
-    } // of isset if statment
-
-    if ($errorMsg === "") {
-        echo '
-                <div class="result">
-                    <h2>Converted Temp</h2>
-                    <p>'.$temp.' degrees '.$unit_1.' is equal to '.number_format($converted_temp, 2).' degrees '.$unit_2.'. Good Day!</p>
-                </div>
-            ';
-    }
-    else {
-        echo '<div class="result">'
-            .$errorMsg.'</div>';
+if ($errorMsg === "") {
+    echo '
+    <div class="result">
+        <h2>Converted Temperature</h2>
+        <p>'.$temp.' degrees '.$unit_1.' is equal to '.number_format($converted_temp, 2).' degrees '.$unit_2.'. <br> Have a good day!</p>
+        </div>';
+} else {
+    echo '<div class="result">'.$errorMsg.'</div>';
     }
 
 } // SERVER REQUEST
